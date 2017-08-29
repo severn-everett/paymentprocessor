e9ccb3c0aa784ee49790743ec868e247b1d05915
severn-everett <severn.everett@gmail.com>
2017-08-29 14:45:40 +0200

    Initial code for Payment Processor REST service

---
+++ src/main/java/com/severett/paymentprocessor/PaymentProcessor.java
@@ -0,0 +1,12 @@
+package com.severett.paymentprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProcessor {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentProcessor.class, args);
+	}
+}
